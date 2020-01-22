54645815e4095e9458c8c48dfaa3f1005783ee45
Hardik Singh <hardiksingh297@gmail.com>
2020-01-22 15:36:26 +0530

    Created the Basic info cards.

--- src/Components/Begin.js
+++ src/Components/Begin.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ParticlesBg from 'particles-bg';
-import {Container,Row,Col,Card,Jumbotron} from 'react-bootstrap';
+import {Container,Row,Col,Card,Jumbotron, Accordion} from 'react-bootstrap';
 import './Begin.css'
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -19,9 +19,12 @@ class Begin extends React.Component{
                             <Card>
                                 <Card.Body>
                                     <Card.Title id="card-heading">What is GSOC?</Card.Title>
+                                        <hr />
                                     <Card.Text>
-                                        Google Summer of Code is a global program focused on bringing more student developers into open source software development. 
-                                        Students work with an open source organization on a 3 month programming project during their break from school.
+                                        <p>
+                                            Google Summer of Code is a global program focused on bringing more student developers into open source software development. 
+                                            Students work with an open source organization on a 3 month programming project during their break from school.
+                                        </p>
                                     </Card.Text>
                                 </Card.Body>
                             </Card>
@@ -30,8 +33,11 @@ class Begin extends React.Component{
                             <Card>
                                 <Card.Body>
                                     <Card.Title id="card-heading">Origin of GSOC</Card.Title>
+                                    <hr />
                                     <Card.Text>
-                                        
+                                        <p>
+                                            Google Summer of Code began in 2005 as a complex experiment with a simple goal: helping students find work related to their academic pursuits during their school holidays.
+                                        </p>
                                     </Card.Text>
                                 </Card.Body>
                             </Card>
@@ -40,6 +46,51 @@ class Begin extends React.Component{
                             <Card>
                                 <Card.Body>
                                     <Card.Title id="card-heading">Benefits of GSOC</Card.Title>
+                                    <hr />
+                                    <Card.Text>
+                                        <p>
+                                            Cracking GSOC offers the following Advantages:
+                                        </p>
+                                        <Accordion>
+                                            <Card>
+                                                <Accordion.Toggle as={Card.Header} eventKey="0">
+                                                    Community
+                                                </Accordion.Toggle>
+                                                <Accordion.Collapse eventKey="0">
+                                                    <Card.Body>
+                                                        <p id="benefits">
+                                                            Helps you in networking and gaining contacts.
+                                                        </p>
+                                                    </Card.Body>
+                                                </Accordion.Collapse>
+                                            </Card>
+                                            <Card>
+                                                <Accordion.Toggle as={Card.Header} eventKey="1">
+                                                    Career
+                                                </Accordion.Toggle>
+                                                <Accordion.Collapse eventKey="1">
+                                                    <Card.Body>
+                                                        <p id="benefits">
+                                                            GSOC is quite competitive.<br />
+                                                            Companies pay a lot of attention to this.
+                                                        </p>
+                                                    </Card.Body>
+                                                </Accordion.Collapse>
+                                            </Card>
+                                            <Card>
+                                                <Accordion.Toggle as ={Card.Header} eventKey="2">
+                                                    Chance with Google itself.
+                                                </Accordion.Toggle>
+                                                <Accordion.Collapse eventKey="2">
+                                                    <Card.Body>
+                                                        <p id="benefits">
+                                                            One time referral given to Google Summer of Code students can be used for a possible job opportunity at Google itself.
+                                                        </p>
+                                                    </Card.Body>
+                                                </Accordion.Collapse>
+                                            </Card>
+                                        </Accordion>
+                                    </Card.Text>
                                 </Card.Body>
                             </Card>
                         </Col>
