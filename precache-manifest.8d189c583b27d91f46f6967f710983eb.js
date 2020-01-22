a7ea47be55106c871048ae5d2bf69d6a81fb8987
Hardik Singh <hardiksingh297@gmail.com>
2020-01-22 22:32:31 +0530

    Updates

---
+++ precache-manifest.8d189c583b27d91f46f6967f710983eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f2d9d94556d0c61495b12ef85f934e",
+    "url": "/GSOC_Landing/index.html"
+  },
+  {
+    "revision": "7b374dd6e2f3e02109ad",
+    "url": "/GSOC_Landing/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "e679b211e46e4c693118",
+    "url": "/GSOC_Landing/static/css/main.267dfc67.chunk.css"
+  },
+  {
+    "revision": "7b374dd6e2f3e02109ad",
+    "url": "/GSOC_Landing/static/js/2.faa8b45d.chunk.js"
+  },
+  {
+    "revision": "7be137dece398f167aee13b4da45c9be",
+    "url": "/GSOC_Landing/static/js/2.faa8b45d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e679b211e46e4c693118",
+    "url": "/GSOC_Landing/static/js/main.c1951aee.chunk.js"
+  },
+  {
+    "revision": "f2625a84fe51fc8f398c",
+    "url": "/GSOC_Landing/static/js/runtime-main.cd98bc0b.js"
+  },
+  {
+    "revision": "a4340e3a08347d9112c06fda1241aa64",
+    "url": "/GSOC_Landing/static/media/mentor.a4340e3a.jpeg"
+  }
+]);
